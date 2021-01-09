ae4aaf993761fc57c0964b61656fcb407f543765
Xin4s <70570587+Xin4s@users.noreply.github.com>
2021-01-09 15:51:31 +0100

    ajout d'une navbar

--- wishlist/src/vue/VueParticipant.php
+++ wishlist/src/vue/VueParticipant.php
@@ -34,7 +34,20 @@ class VueParticipant
         $html = <<<END
         <!DOCTYPE html> 
         <html lang="fr">
+            <header>
+            <title>MyWishlist</title>
+            </header>
             <body>
+                <nav class="navbar navbar=inverse navbar-static-top">
+                    <div class="container">
+                        <div class="navbar-header">
+                            <a href="a" class="navbar-brand">Slim3</a>
+                        </div>
+                        <div class="nav navbar-nav">
+                            <li><a href="a">liste de souhaits</a></li>
+                        </div>
+                    </div>
+                </nav>
                 <div class="content">
                  $content
                 </div>
