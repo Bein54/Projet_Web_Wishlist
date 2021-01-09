58536c37d0c2ed265e8b0f078a9b105fd6fc97fc
leonard <leonard67130.schmitt@gmail.com>
2021-01-09 15:12:18 +0100

    ajout vue

--- wishlist/src/vue/VueParticipant.php
+++ wishlist/src/vue/VueParticipant.php
@@ -2,7 +2,46 @@
 
 namespace wishlist\vue;
 
+const TEST = 0;
+const VUE_PARTICIPANT = 1;
+
 class VueParticipant
 {
-    
+    private $selecteur;
+    private $elem;
+
+    public function __construct($elem, $selecteur)
+    {
+        $this->$selecteur = $selecteur;
+        $this->elem = $elem;
+    }
+
+    function renderer()
+    {
+        $content = '';
+
+        switch ($this->selecteur) {
+            case TEST:
+            {
+                $content = '<br>TEST<br>';
+            }
+            case VUE_PARTICIPANT:
+            {
+
+            }
+        }
+
+        $html = <<<END
+        <!DOCTYPE html> 
+        <html lang="fr">
+            <body>
+                <div class="content">
+                 $content
+                </div>
+            </body>
+        </html>
+        END;
+
+        return $html;
+    }
 }
