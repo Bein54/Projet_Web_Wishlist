4892f594d707567ed5c67108a07095433f2cd302
julienferaux <julien1.feraux@yahoo.fr>
2021-01-15 15:01:35 +0100

    rien

--- wishlist/src/controleur/ControleurParticipation.php
+++ wishlist/src/controleur/ControleurParticipation.php
@@ -4,10 +4,20 @@
 namespace wishlist\controleur;
 
 
+use http\Env\Response;
 use wishlist\models\Item;
 use wishlist\models\Liste;
+use wishlist\vue\VueParticipant;
 
 class ControleurParticipation
 {
+    private $c;
 
+    public function __construct(\Slim\Container $c){
+        $this->c = $c;
+    }
+    function fairQQC (Request $rq,Response $rs, array $args): Response{
+        $path = $rq->getURI()->getBasePath();
+
+    }
 }
